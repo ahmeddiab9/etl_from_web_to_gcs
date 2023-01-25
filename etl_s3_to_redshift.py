7816e57c9e659e82e68c7169e6512e1a0f8f44a8
ahmeddiab9 <81594449+ahmeddiab9@users.noreply.github.com>
2023-01-25 02:31:24 +0200

    enhance

--- etl_s3_to_redshift.py
+++ etl_s3_to_redshift.py
@@ -8,6 +8,7 @@ import pyarrow.parquet as pq
 import s3fs
 s3 = s3fs.S3FileSystem()
 from red_panda import RedPanda
+from sqlalchemy import create_engine
 
 # aws_credentials_block = AwsCredentials.load("etl-from-web-to-s3")
 
@@ -33,25 +34,8 @@ def transform(path: Path) -> pd.DataFrame:
 @task()
 def write_redshift(df : pd.DataFrame) -> None:
     """Write data to Redshift"""
-    # user admin , password Admin123
-    # Workgroup name etl-from-s3
-    # Namespace name etl-from-s3
-    # Database name dev
-    redshift_conf = {
-    "user": "admin",
-    "password": "Admin123",
-    "dbname": "dev",
-    }
-    aws_conf = {
-    "aws_access_key_id": "AKIATYWOATBH4UM7NYWB",
-    "aws_secret_access_key": "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",
-    }
-    rp = RedPanda(redshift_conf, aws_conf)
-    rp = RedPanda(redshift_conf, aws_conf)
-    s3_bucket = "prefect-etl-to-s3"
-    s3_file_name = "prefect-etl-to-s3\data\yellow\yellow_tripdata_2021-01.parquet" # optional, randomly generated if not provided
-    rp.df_to_redshift(df, "table_name", bucket=s3_bucket, path=s3_file_name, append=False)
-    return
+    #name_space etl-from-s3-zoom
+    # https://stackoverflow.com/questions/38402995/how-to-write-data-to-redshift-that-is-a-result-of-a-dataframe-created-in-python
 
 @flow()
 def etl_ws3_to_redshift() -> None:
